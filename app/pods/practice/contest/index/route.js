23939a4879bce14fe29cb4339ba19257aedb4ddc
Jatin Katyal <jatin.katyal13@gmail.com>
2019-07-12 15:12:58 +0530

    generalised components

--- app/pods/practice/contest/index/route.js
+++ app/pods/practice/contest/index/route.js
@@ -1,12 +1,31 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp'
+import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
+  @service store
+
   model() {
-    return this.modelFor('practice.contest')
+    const practice = this.modelFor('practice.contest').practice
+    const contest = this.modelFor('practice.contest').contest
+    const leaderboard = this.store.query('contest_leaderboard', {
+      filter: {
+        contestId: contest.get('id')
+      },
+      sort: '-score,time',
+      include: 'college,user',
+      exclude: 'college.*,user.*'
+    })
+    return RSVP.hash({
+      practice,
+      contest,
+      leaderboard
+    })
   }
 
   setupController(controller, model) {
     controller.set('practice', model.practice)
     controller.set('contest', model.contest)
+    controller.set('leaderboard', model.leaderboard)
   }
 }
