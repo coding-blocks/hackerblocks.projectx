885bb6e4da66f6c161f36392da021adba856ceda
Jatin Katyal <jatin.katyal13@gmail.com>
2019-07-30 17:31:46 +0530

    dashboard initiated

--- app/pods/components/practice-contest-card/component.js
+++ app/pods/components/practice-contest-card/component.js
@@ -1,4 +1,22 @@
 import Component from '@ember/component';
+import { restartableTask } from 'ember-concurrency-decorators';
+import { inject as service } from '@ember/service';
+import { alias } from '@ember/object/computed';
 
-export default Component.extend({
-});
+export default class PracticeContestCard extends Component {
+  @service store
+  @alias('fetchLevelTask.lastSuccessful.value') currentLevel
+
+  didReceiveAttrs() {
+    this.fetchLevelTask.perform()
+  }
+
+  @restartableTask fetchLevelTask = function *() {
+    const levels = yield this.store.query('user_level', {
+      filter: {
+        contestId: this.practice.belongsTo('contest').id()
+      }
+    })
+    return levels.toArray()[0]
+  }
+}
