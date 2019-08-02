d9bf5022db0cac410d2b759ef8f67873db919981
Jatin Katyal <jatin.katyal13@gmail.com>
2019-08-02 19:43:55 +0530

    fix: review pad

--- app/pods/components/question-pill/component.js
+++ app/pods/components/question-pill/component.js
@@ -2,7 +2,7 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 export default class QuestionPill extends Component {
-  @computed('submissions')
+  @computed('submissions.@each')
   get color() {
     return this.submissions.findBy('question_id', this.question_id) ?
     'bg-green':
