119d31196ea35dfda4c9f1640e77c7501c70679b
Jatin Katyal <jatin.katyal13@gmail.com>
2019-08-03 18:56:35 +0530

    add: editorial

--- app/pods/components/solution-component/component.js
+++ app/pods/components/solution-component/component.js
@@ -1,17 +1,33 @@
 import Component from '@ember/component';
 import { restartableTask } from 'ember-concurrency-decorators';
 import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
+import { alias } from '@ember/object/computed';
 
 export default class SolutionComponent extends Component {
   @service store
+  @alias('fetchEditorialTask.lastSuccessful.value') editorial
 
-  @computed('problem.editorial')
-  get editorial() {
-    return this.problem.editorial
+  didReceiveAttrs() {
+    this.fetchEditorialTask.perform()
   }
 
   @restartableTask unlockEditorialTask = function *() {
-    // TODO
+    const unlockedEditorial = this.store.createRecord('unlocked-editorial', {
+      contest: this.contest,
+      problem: this.problem
+    })
+    yield unlockedEditorial.save()
+    this.fetchEditorialTask.perform()
+  }
+
+  @restartableTask fetchEditorialTask = function *() {
+    return yield this.store.queryRecord('editorial', {
+      custom: {
+        ext: 'url',
+        url: 'current-editorial'
+      },
+      contest_id: this.contest.id,
+      problem_id: this.problem.id
+    })
   }
 }
