1a921a87552209fe8831caee6e4f96e57e30b37f
Jatin Katyal <jatin.katyal13@gmail.com>
2019-08-11 00:02:18 +0530

    correct contest count on public contest view

--- app/pods/contests/index/route.js
+++ app/pods/contests/index/route.js
@@ -1,25 +1,3 @@
 import Route from '@ember/routing/route';
 
-export default class IndexRoute extends Route {
-  queryParams = {
-    limit: 20,
-    offset: 0
-  }
-
-  model() {
-    return this.store.query('contest', {
-      custom: {
-        ext: 'url',
-        url: 'public'
-      },
-      page: {
-        limit: this.limit,
-        offset: this.offset
-      }
-    })
-  }
-
-  setupController(controller, model) {
-    controller.set('contests', model)
-  }
-}
+export default class IndexRoute extends Route {}
