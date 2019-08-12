311a4548726cd612dbe9cb2f95bbb3cb0dcd0e11
vdvibhu20 <vibhu.dujari@codingblocks.com>
2019-08-13 00:29:53 +0530

    dcb fixes

--- app/pods/dcb/problem/controller.js
+++ app/pods/dcb/problem/controller.js
@@ -1,3 +1,24 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
-export default class ProblemController extends Controller {}
+export default class ProblemController extends Controller {
+  @action
+  async toggleBookmark(problem) {
+    const bookmark = await problem.get('bookmark')
+    if (bookmark) {
+      await bookmark.destroyRecord()
+      return problem.set('bookmarkedBy', null)
+    }
+    const bookmarkProblem = this.store.createRecord('bookmarked-problem', {
+      problem,
+      contest: this.contest,
+      contentTypeId: this.dcb.id
+    })
+
+    bookmarkProblem.save()
+  }
+
+  @action onTimerEnd() {
+    window.location.reload()
+  }
+}
