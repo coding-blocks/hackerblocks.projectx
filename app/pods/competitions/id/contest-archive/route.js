09d557d59b0cae1c9ef18ffedde8e420ea7e59db
Abhishek Gupta <hi@itsabhishek.me>
2019-08-13 02:38:05 +0530

    can resume old competitions contests

---
+++ app/pods/competitions/id/contest-archive/route.js
@@ -0,0 +1,37 @@
+import Route from '@ember/routing/route';
+import RSVP from 'rsvp'
+
+export default Route.extend({
+  async model (params) {
+    const contest = await this.store.findRecord('contest', params.contestId)
+    
+    let contest_attempt = await this.store.queryRecord('contest-attempt', {
+      custom: {
+        ext: 'url',
+        url: 'current-attempt'
+      },
+      contest_id: params.contestId
+    })
+
+    if (!contest_attempt) {
+      contest_attempt = this.store.createRecord('contest-attempt', {
+        contest
+      })
+      await contest_attempt.save()
+    }
+
+    const competition = this.modelFor('competitions.id')
+
+    return RSVP.hash({
+      contest,
+      contest_attempt,
+      competition
+    })
+  },
+
+  afterModel(model) {
+    this.set('breadCrumb', {
+      title: model.contest.name
+    })
+  }
+});
--- app/pods/competitions/id/index/controller.js
+++ app/pods/competitions/id/index/controller.js
@@ -4,6 +4,6 @@ import { action } from '@ember/object';
 export default class Index extends Controller {
   @action
   onParticipate(contest_id) {
-    this.transitionToRoute('competitions.id.contest', contest_id)
+    this.transitionToRoute('competitions.id.contest-archive', contest_id)
   }
 }
