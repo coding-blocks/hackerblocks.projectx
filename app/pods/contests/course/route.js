486531e817a18c1f0467dff053788a7f210ee52f
Abhishek Gupta <hi@itsabhishek.me>
2019-08-14 03:14:56 +0530

    fix admission contests

--- app/pods/contests/course/route.js
+++ app/pods/contests/course/route.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model(){
     return this.store.findAll('course', {
-      include: 'course_contest'
+      include: 'course_contests'
     })
   }
 });
