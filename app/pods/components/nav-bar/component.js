45332d5295c1cca2933201efd2cab9714cbdda22
vdvibhu20 <vibhu.dujari@codingblocks.com>
2019-08-15 17:24:58 +0530

    mobile changes

--- app/pods/components/nav-bar/component.js
+++ app/pods/components/nav-bar/component.js
@@ -1,9 +1,18 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service'
 import { alias } from '@ember/object/computed'
+import { action } from '@ember/object';
 
 export default class NavBarComponent extends Component {
   @service session
   @service currentUser
   @alias('currentUser.user') user
+
+  hideHamburgerNav = true
+
+  @action 
+  toggleHamburgerNav() {
+    console.log('arreey toh')
+    this.toggleProperty('hideHamburgerNav')
+  }
 }
