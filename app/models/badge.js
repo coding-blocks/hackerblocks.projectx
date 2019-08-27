094cc78fb35dfc94c1bb59859876050aa7c4a499
vdvibhu20 <vibhu.dujari@codingblocks.com>
2019-08-27 13:55:34 +0530

    badge metrics

--- app/models/badge.js
+++ app/models/badge.js
@@ -1,9 +1,11 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 const { Model } = DS;
 
 export default Model.extend({
   metric: DS.attr(),
   icon: DS.attr(),
   createdAt: DS.attr(),
-  contest: DS.belongsTo('contest', {inverse: null})
+  contest: DS.belongsTo('contest', {inverse: null}),
+  theme: DS.attr()
 });
