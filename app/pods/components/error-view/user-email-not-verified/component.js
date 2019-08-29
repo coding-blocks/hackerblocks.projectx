6eb96cec81672eecdef00abb79c3d9d50644a245
Jatin Katyal <jatin.katyal13@gmail.com>
2019-08-29 23:07:23 +0530

    template updates

--- app/pods/components/error-view/user-email-not-verified/component.js
+++ app/pods/components/error-view/user-email-not-verified/component.js
@@ -5,6 +5,13 @@ import { inject as service } from '@ember/service';
 export default class NotVerifiedComponent extends Component {
   @service currentUser
   @service session
+  @service api
+
+  @action
+  async sendVerifyEmail() {
+    await this.api.request('oneauth/verifyemail')
+    this.set('verification_sent', true)
+  }
 
   @action
   async reloadUser() {
