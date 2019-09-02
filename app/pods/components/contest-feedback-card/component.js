b1cdf139727f68dc757d93f2dba103b67f76f5bf
vdvibhu20 <vibhu.dujari@codingblocks.com>
2019-09-02 18:11:26 +0530

    contest feedback submit buffer

--- app/pods/components/contest-feedback-card/component.js
+++ app/pods/components/contest-feedback-card/component.js
@@ -1,10 +1,13 @@
 import Component from '@ember/component';
 import { dropTask } from 'ember-concurrency-decorators';
-import { timeout } from 'ember-concurrency'
+import { timeout } from 'ember-concurrency';
+import { later } from '@ember/runloop';
 
 export default class ContestFeedbackCard extends Component {
   @dropTask saveFeedbackTask = function *() {
-    yield this.feedback.save()
+    later(() => {
+      this.feedback.save()
+    }, (Math.random() * 15) * 1000)
     yield timeout(2000)
     this.afterSave()
   }
