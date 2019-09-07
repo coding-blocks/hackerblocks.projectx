a7d12689781139906e01afed02b41022fc49e362
Jatin Katyal <jatin.katyal13@gmail.com>
2019-09-07 11:26:00 +0530

    import router

--- app/pods/login/route.js
+++ app/pods/login/route.js
@@ -1,7 +1,10 @@
 import Route from '@ember/routing/route';
 import env from 'hackerblocks/config/environment';
+import { inject as service } from '@ember/service';
 
 export default class LoginRoute extends Route {
+  @service router
+  
   loginUrl = `${env.oneauthURL}/oauth/authorize?response_type=code&client_id=${
     env.clientId
   }&redirect_uri=${env.publicUrl}`; 
