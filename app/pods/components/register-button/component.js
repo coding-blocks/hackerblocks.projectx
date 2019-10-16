6fca52f064b11eee1177378b9a3c5e2b258dbc14
Jatin Katyal <jatin.katyal13@gmail.com>
2019-10-16 17:34:18 +0530

    success message

--- app/pods/components/register-button/component.js
+++ app/pods/components/register-button/component.js
@@ -1,18 +1,19 @@
 import Component from '@ember/component';
 import { restartableTask } from 'ember-concurrency-decorators';
 import { inject as service } from '@ember/service';
+import { later } from '@ember/runloop';
 
 export default class RegisterButtonComponent extends Component {
-  @service store;
+  @service store
 
   @restartableTask createRegistrationTask = function *(form) {
     yield this.store.createRecord('contest-registration', {
       contest: this.contest,
       form
     }).save()
-    this.set('showRegistrationForm', false)
+    this.set('success', true)
     if (this.onAfterSave) {
-      this.onAfterSave()
+      later(this.onAfterSave, 2000)
     }
   }
 }
