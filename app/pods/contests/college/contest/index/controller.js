f15ff2ac7f3741b1f03db2e5c449538fd222df64
Jatin Katyal <jatin.katyal13@gmail.com>
2020-02-17 14:34:21 +0530

    deprecate router js

--- app/pods/contests/college/contest/index/controller.js
+++
@@ -1,9 +0,0 @@
-import Controller from '@ember/controller';
-import { action } from '@ember/object';
-
-export default class ContestIndexController extends Controller{
-  @action
-  onAfterCreate(){
-    this.transitionToRoute('contests.college.contest.attempt')
-  }
-}
--- app/router.js
+++ app/router.js
@@ -14,12 +14,6 @@ Router.map(function() {
         this.route('upcoming');
         this.route('archived');
       });
-      this.route('contest', { path: '/c/:college_contest_id'}, function() {
-        this.route('attempt', function() {
-          this.route('problem', { path: '/p/:problem_id' });
-        });
-      });
-
       this.route('attempt', function() {});
     });
     this.route('admission', function() {
@@ -27,26 +21,12 @@ Router.map(function() {
         this.route('upcoming');
         this.route('archived');
       });
-      this.route('contest', {path: '/c/:admission_contest_id'}, function() {
-        this.route('attempt', function() {
-          this.route('problem', {path: '/p/:problem_id'});
-          this.route('quiz', {path: '/q/:quiz_id'});
-        });
-        this.route('feedback');
-      });
       this.route('apat', function() {
         this.route('archived');
         this.route('upcoming');
       });
     });
-
-    this.route('admissions', function() {
-      this.route('case', function() {});
-    });
     this.route('course', function() {
-      this.route('contest', { path: '/c/:course_contest_id'}, function() {
-        this.route('problem', { path: '/p/:problem_id' });
-      });
       this.route('id', { path: '/:course_id'});
     });
     this.route('hiring', function() {
@@ -54,15 +34,7 @@ Router.map(function() {
         this.route('archived');
         this.route('upcoming');
       });
-      this.route('contest', { path: '/c/:hiring_contest_id'}, function() {
-        this.route('attempt', function() {
-          this.route('problem', {path: '/p/:problem_id'});
-          this.route('quiz', {path: '/q/:quiz_id'});
-        });
-        this.route('feedback');
-      });
     });
-
     this.route('contest', {path: '/:contest_id'}, function() {
       this.route('problem', {path: '/p/:problem_id'});
       this.route('attempt', function() {
@@ -89,12 +61,6 @@ Router.map(function() {
   this.route('problem');
   this.route('competitions', function() {
     this.route('id', {path: '/:competition_id'}, function() {
-      this.route('contest', {path: '/c/:contest_id'}, function() {
-        this.route('attempt', function() {
-          this.route('problem', {path: '/p/:problem_id'});
-        });
-        this.route('feedback');
-      });
       this.route('contest-archive', {path: '/archive/:contestId'} ,function() {
         this.route('problem', {path: '/p/:problemId'});
       });
