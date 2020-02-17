0f282183473de5bdfe848cbadd84f3ecb5494f6b
Jatin Katyal <jatin.katyal13@gmail.com>
2020-02-17 14:29:26 +0530

    initiated model

---
+++ app/models/content.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+const { Model } = DS;
+
+export default Model.extend({
+  name: DS.attr(),
+  type: DS.attr(),
+  problem: DS.belongsTo('problem'),
+  quiz: DS.belongsTo('quiz')
+});
