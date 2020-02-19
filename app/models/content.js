a3b6e00e65cd70b81daf650095966b154d6f2d03
Jatin Katyal <jatin.katyal13@gmail.com>
2020-02-20 01:51:20 +0530

    difficulty denormalized

--- app/models/content.js
+++ app/models/content.js
@@ -4,6 +4,7 @@ const { Model } = DS;
 export default Model.extend({
   name: DS.attr(),
   type: DS.attr(),
+  difficulty: DS.attr(),
   problem: DS.belongsTo('problem'),
   quiz: DS.belongsTo('quiz')
 });
