5b805d27bf25f4971dbdcc8f8c4eba8cc645d4a9
Jatin Katyal <jatin.katyal13@gmail.com>
2020-02-28 19:35:22 +0530

    quiz submission flow

--- app/pods/components/code-editor-component/component.js
+++ app/pods/components/code-editor-component/component.js
@@ -1,18 +1,18 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { task, taskGroup } from 'ember-concurrency-decorators';
-import { timeout } from 'ember-concurrency';
-import { later } from '@ember/runloop';
 import { computed } from '@ember/object';
-import { Base64 } from 'js-base64';
-import { alias } from '@ember/object/computed';
+import { alias, equal } from '@ember/object/computed';
 
 export default class CodeEditorComponent extends Component {
   @service api
   @service store
+  @service submission
 
-  @alias('judgeTaskGroup.lastSuccessful.value.judge_result') lastResult
+  @alias('submission.codeTaskGroup.lastSuccessful.value.judge_result') lastResult
+  @equal('submission.codeTaskGroup.last.error.status', 429) submitSpam
 
+  badge = null
   showAwardedBadge = false
 
   @computed('problem.id', 'contest.id')
@@ -22,85 +22,15 @@ export default class CodeEditorComponent extends Component {
     }
   }
 
-  @taskGroup({ drop: true }) judgeTaskGroup
-
-  @task({ group: 'judgeTaskGroup' }) onRunTask = function*(language, code, input) {
-    try {
-      const response = yield this.api.request('submissions/run', {
-        method: 'POST',
-        data: {
-          content_id: this.content.id,
-          input: Base64.encode(input),
-          source: Base64.encode(code),
-          language
-        }
-      })
-      let maxTries = 30
-      while(maxTries--) {
-        yield timeout(2000)
-        const submission = yield this.store.findRecord('submission', response.submissionId, { refresh: true })
-        if (submission.judge_result){
-          return submission
-        }
-      }
-      return null
-    } catch (err) {
-      this.set('resultComponent', '')
-      if (err.status == 429) {
-        this.set('submitSpam', true)
-        later(() => this.set('submitSpam', false), 10000)
-      }
-    }
+  didReceiveAttrs() {
+    this.submission.initialize(this.contest, this.content)
   }
 
-  @task({ group: 'judgeTaskGroup' }) onSubmitTask = function*(language, code) {
-    try {
-      const response = yield this.api.request('submissions/submit', {
-        method: 'POST',
-        data: {
-          contest_id: this.contest.id,
-          content_id: this.content.id,
-          source: Base64.encode(code),
-          language
-        }
-      })
-  
-      let maxTries = 30
-      while(maxTries--) {
-        yield timeout(2000)
-        const submission = yield this.store.findRecord('submission', response.submissionId, {
-          refresh: true,
-          include: 'badge'
-        })
-        if (submission.get('badge.id')) {
-          this.set('badge', submission.get('badge'))
-          this.set('showAwardedBadge', true)
-        }
-        if (submission.judge_result){
-          if (this.fullScreen) {
-            const score = +submission.score
-            const progress = yield this.problem.get('progress')
-            if (progress.get('status') === 'done') {
-              return
-            }
-            if (score === 100) {
-              progress.set('status', 'done')
-            } else if (score > 0 && score < 100) {
-              progress.set('status', 'undone')
-            } else {
-              progress.set('status', 'failed')
-            }
-            progress.save()
-          }
-          return submission
-        }
-      }
-      return null
-    } catch (err) {
-      if (err.status == 429) {
-        this.set('submitSpam', true)
-        later(() => this.set('submitSpam', false), 10000)
-      }
+  @task submitCodeTask = function *(language, code) {
+    yield this.submission.submitCodeTask.perform(language, code)
+    if (this.lastResult.get('badge.id')) {
+      this.set('badge', this.lastResult.get('badge'))
+      this.set('showAwardedBadge', true)
     }
   }
 }
--- app/pods/contests/contest/content/quiz/controller.js
+++ app/pods/contests/contest/content/quiz/controller.js
@@ -1,6 +1,18 @@
 import Controller from '@ember/controller';
+import { dropTask } from 'ember-concurrency-decorators';
+import { inject as service } from '@ember/service';
+import { alias } from '@ember/object/computed'
 
 export default class Quiz extends Controller {
+  @service submission
+
+  @alias('submitQuizTask.lastSuccessful.value.judge_result') lastResult
+
   queryParams = ['q']
   q = 1
+
+  @dropTask submitQuizTask = function *() {
+    this.submission.initialize(this.contest, this.content)
+    return this.submission.submitQuizTask.perform()
+  }
 }
