ee81b0ccc73603acac52a8672c4cb5ad8d13753e
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2020-03-24 00:05:00 +0530

    design updated for contest page
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- app/pods/contests/index/controller.js
+++ app/pods/contests/index/controller.js
@@ -1,6 +1,7 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { restartableTask } from 'ember-concurrency-decorators';
+import moment from 'moment'
 
 export default class IndexController extends Controller {
   @service api
@@ -17,8 +18,22 @@ export default class IndexController extends Controller {
     this.fetchModelCount.perform('college-contests')
   }
 
-  @restartableTask fetchModelCount = function *(model) {
+  @restartableTask fetchModelCount = function* (model) {
     const { count } = yield this.api.request(`${model}/count`)
     this.set(`contestCount.${model}`, count)
+  }
+
+  @restartableTask fetchContests = function* () {
+    const params = {
+      page: { limit: 6 },
+      filter: {
+        accept_registrations: true,
+        start_time: {
+          $gt: moment().toISOString()
+        }
+      }
+    }
+
+    return yield this.store.query('contest', params)
   }
 }
