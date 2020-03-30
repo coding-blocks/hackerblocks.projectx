d5d87c98b10141aea2adaa4696bb16c8a60fc0bc
Jatin Katyal <jatin.katyal13@gmail.com>
2020-03-30 16:15:24 +0530

    archived competition contest fix

--- app/pods/components/archived-competition-contest-card/component.js
+++ app/pods/components/archived-competition-contest-card/component.js
@@ -1,9 +1,3 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 
-export default class ArchivedContestCard extends Component {
-  @computed('contest')
-  get problemCount() {
-    return ("0" + this.contest.hasMany('problems').ids().length).slice(-2)
-  }
-}
+export default class ArchivedContestCard extends Component {}
