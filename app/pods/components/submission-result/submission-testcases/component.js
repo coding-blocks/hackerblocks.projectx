ff13a5164611fe410c49dfe4d1f74df740c3493d
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2020-04-15 20:23:16 +0530

    test UI
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- app/pods/components/submission-result/submission-testcases/component.js
+++ app/pods/components/submission-result/submission-testcases/component.js
@@ -5,7 +5,8 @@ export default class SubmissionTestcases extends Component {
   selectedIndex = 0
 
   @computed('selectedIndex')
-  get compilerMessage() {    
+  get compilerMessage() {
+    console.log(this.testcases)    
     return this.testcases[this.selectedIndex].result
   }
 }
