be14d968b0b2a71c969cb12e5b1d51caed3fdd7b
Jatin Katyal <jatin.katyal13@gmail.com>
2020-06-16 18:08:50 +0530

    design fixes for project view

--- app/pods/components/project-view/component.js
+++ app/pods/components/project-view/component.js
@@ -1,51 +1,41 @@
-import Component from "@ember/component";
-import { inject as service } from "@ember/service";
-import { dropTask } from "ember-concurrency-decorators";
-import { alias } from "@ember/object/computed";
-import { action } from "@ember/object";
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { dropTask } from 'ember-concurrency-decorators';
+import { alias } from '@ember/object/computed';
+import { action } from '@ember/object';
 
 export default class ProjectViewComponent extends Component {
   @service store;
   @service api;
-
   @service submission;
 
   source_url = null;
   uploading = false;
-  uploadError = false;
-  doneUploading = false;
-  Trigger = false;
-  ProgressPercentage = 0;
-  new_expected_output = "Loading...";
-  selectedTab = "problem";
+  uploadTrigger = false;
+  uploadPercentage = 0;
+  selectedTab = 'problem';
 
   @alias('submitProjectTask.lastSuccessful.value.judge_result') lastResult
   
   @action triggerUpload() {
-    this.set("uploadError", false);
-    this.set("doneUploading", false);
-    this.set("Trigger", true);
+    this.set('uploading', true)
+    this.set('uploadTrigger', true);
   }
-
-  @action Progress(e) {
-    this.set("ProgressPercentage", e.percent);
+  @action onProgress(e) {
+    this.set('uploadPercentage', e.percent);
   }
-
   @action didSignUpload(e) {
-    this.set("source_url", e.url);
+    this.set('uploading', false)
+    this.set('source_url', e.url);
   }
-
-  @action didUploadFile() {
-    this.set("new_expected_output", null);
-    this.set("doneUploading", true);
-  }
-
   @action didError() {
-    this.set("uploading", false);
-    this.set("Trigger", false);
+    this.set('uploading', false)
+    this.set('uploadTrigger', false)
   }
 
   @dropTask submitProjectTask = function* () {
-    return this.submission.submitProjectTask.perform(this.source_url);
+    const result = yield this.submission.submitProjectTask.perform(this.source_url);
+    this.set('source_url', null)
+    return result
   };
 }
