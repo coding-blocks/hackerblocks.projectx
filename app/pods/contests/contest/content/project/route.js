81a49f90e3b216ceb8e870b431b3cd51fb1af9c0
Jatin Katyal <jatin.katyal13@gmail.com>
2020-11-14 03:37:06 +0530

    setup controller attrs in route for project

--- app/pods/contests/contest/content/project/route.js
+++ app/pods/contests/contest/content/project/route.js
@@ -1,4 +1,21 @@
 import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
 
-export default Route.extend({
-});
+export default class ProjectRoute extends Route{
+  model() {
+    const { contest } = this.modelFor('contests.contest')
+    const { content } = this.modelFor('contests.contest.content')
+    const project = content.get('project')
+
+    return hash({
+      contest,
+      content,
+      project
+    })
+  }
+  setupController(controller, model) {
+    controller.set('contest', model.contest);
+    controller.set('content', model.content);
+    controller.set('project', model.project);
+  }
+};
