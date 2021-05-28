9195cded65879605176a2e7d629786af94feea04
Aditya <aditya2041999@gmail.com>
2021-05-28 20:43:52 +0530

    fixed

--- app/models/submission.js
+++ app/models/submission.js
@@ -1,7 +1,7 @@
-import Ember from 'ember';
-import moment from 'moment';
-import { get } from '@ember/object'
-import DS from 'ember-data';
+import Ember from "ember";
+import moment from "moment";
+import { get } from "@ember/object";
+import DS from "ember-data";
 const { Model } = DS;
 
 export default Model.extend({
@@ -9,80 +9,87 @@ export default Model.extend({
   submit_at: DS.attr(),
   msecs_elapsed: DS.attr(),
   language: DS.attr(),
-  score: DS.attr('number'),
+  score: DS.attr("number"),
   result: DS.attr(),
   tc_runs: DS.attr(),
   judge_result: DS.attr(),
   explanation: DS.attr(),
   is_top_submission: DS.attr(),
   plagiarism_detected: DS.attr(),
-  content: DS.belongsTo('content'),
-  contest: DS.belongsTo('contest'),
-  user: DS.belongsTo('user'),
+  content: DS.belongsTo("content", { inverse: null }),
+  contest: DS.belongsTo("contest"),
+  user: DS.belongsTo("user"),
   createdAt: DS.attr(),
   submissionType: DS.attr(),
-  badge: DS.belongsTo('badge'),
-  executionTime: Ember.computed('judge_result', function () {
-    const testcases = get(this, 'judge_result.data.testcases')
-    if (!testcases) 
-      return '--'
-    
-    return testcases.reduce((acc, t) => acc + +t.runtime, 0).toFixed(2)
+  badge: DS.belongsTo("badge"),
+  executionTime: Ember.computed("judge_result", function () {
+    const testcases = get(this, "judge_result.data.testcases");
+    if (!testcases) return "--";
+
+    return testcases.reduce((acc, t) => acc + +t.runtime, 0).toFixed(2);
   }),
-  submitAtHumanize: Ember.computed('createdAt', function() {
-    return moment(this.createdAt).fromNow()
+  submitAtHumanize: Ember.computed("createdAt", function () {
+    return moment(this.createdAt).fromNow();
   }),
-  resultParams: Ember.computed('explanation', 'submissionType', function() {
-    if (this.submissionType === 'web') {
+  resultParams: Ember.computed("explanation", "submissionType", function () {
+    if (this.submissionType === "web") {
       return {
-        color: 'green',
-        icon: 'fas fa-check',
-        message: 'Submitted'
-      }
+        color: "green",
+        icon: "fas fa-check",
+        message: "Submitted",
+      };
     } else {
-      switch(this.explanation) {
-        case 'Perfect': return {
-          color: 'green',
-          icon: 'fas fa-check',
-          message: 'Accepted'
-        }
-        case 'WrongAnswer':
-        case 'FailedTestcase': return {
-          color: 'red',
-          icon: 'fas fa-times',
-          message: 'Wrong Answer'
-        }
-        case 'TimeLimitExceeded': return {
-          color: 'orange',
-          icon: 'fas fa-exclamation',
-          message: 'TLE'
-        }
-        case 'CompilationError': return {
-          color: 'orange',
-          icon: 'fas fa-exclamation',
-          message: 'Compilation Error'
-        }
-        case 'ContestOver': return {
-          color: 'orange',
-          icon: 'fas fa-exclamation',
-          message: 'Contest Over'
-        }
-        case 'TestcaseUnlocked': return {
-          color: 'orange',
-          icon: 'fas fa-exclamation',
-          message: 'Test Case Unlocked'
-        }
-        case 'EditorialUnlocked': return {
-          color: 'orange',
-          icon: 'fas fa-exclamation',
-          message: 'Editorial Unlocked'
-        }
-        default: return {
-          color: 'orange',
-          icon: 'fas fa-exclamation',
-          message: 'Submission Not Judged'
-        }
+      switch (this.explanation) {
+        case "Perfect":
+          return {
+            color: "green",
+            icon: "fas fa-check",
+            message: "Accepted",
+          };
+        case "WrongAnswer":
+        case "FailedTestcase":
+          return {
+            color: "red",
+            icon: "fas fa-times",
+            message: "Wrong Answer",
+          };
+        case "TimeLimitExceeded":
+          return {
+            color: "orange",
+            icon: "fas fa-exclamation",
+            message: "TLE",
+          };
+        case "CompilationError":
+          return {
+            color: "orange",
+            icon: "fas fa-exclamation",
+            message: "Compilation Error",
+          };
+        case "ContestOver":
+          return {
+            color: "orange",
+            icon: "fas fa-exclamation",
+            message: "Contest Over",
+          };
+        case "TestcaseUnlocked":
+          return {
+            color: "orange",
+            icon: "fas fa-exclamation",
+            message: "Test Case Unlocked",
+          };
+        case "EditorialUnlocked":
+          return {
+            color: "orange",
+            icon: "fas fa-exclamation",
+            message: "Editorial Unlocked",
+          };
+        default:
+          return {
+            color: "orange",
+            icon: "fas fa-exclamation",
+            message: "Submission Not Judged",
+          };
       }
     }
-  })
+  }),
 });
--- app/pods/components/submissions-heat-map-view/component.js
+++ app/pods/components/submissions-heat-map-view/component.js
@@ -1,51 +1,51 @@
-import Component from '@ember/component';
-import { restartableTask } from 'ember-concurrency-decorators';
-import { inject as service } from '@ember/service';
-import { computed, action } from '@ember/object';
+import Component from "@ember/component";
+import { restartableTask } from "ember-concurrency-decorators";
+import { inject as service } from "@ember/service";
+import { computed, action } from "@ember/object";
 
 export default class SubmissionHeatMapComponent extends Component {
   @service store;
   @service currentUser;
 
-  date = new Date()
-  showModal = false
+  date = new Date();
+  showModal = false;
 
-  @computed('currentUser')
+  @computed("currentUser")
   get userHimself() {
-    return this.currentUser.user.id === this.userId
+    return this.currentUser.user.id === this.userId;
   }
 
   didReceiveAttrs() {
-    this.fetchSubmissionsForDateTask.perform()
+    this.fetchSubmissionsForDateTask.perform();
   }
 
   @restartableTask fetchSubmissionsForDateTask = function* () {
-    const submissions = yield this.store.query('submission', {
+    const submissions = yield this.store.query("submission", {
       custom: {
-        ext: 'url',
-        url: 'date'
+        ext: "url",
+        url: "date",
       },
       date: this.date,
       user_id: this.userId,
-      include: 'problem,contest',
-      exclude: 'problem.*,user.*,contest.*',
-      page: this.page
-    })
-    this.set('submissions', submissions)
-  }
+      include: "problem,contest,content",
+      exclude: "problem.*,user.*,contest.*",
+      page: this.page,
+    });
+    this.set("submissions", submissions);
+  };
 
   @action
-  fetchSubmissionsForDate(date){
-    this.set('date', date)
+  fetchSubmissionsForDate(date) {
+    this.set("date", date);
     //this will trigger two backend requests
     //magar honi ko kaun hi taal sakta hai :|
-    this.setOffset(0)
-    this.fetchSubmissionsForDateTask.perform()
+    this.setOffset(0);
+    this.fetchSubmissionsForDateTask.perform();
   }
 
   @action
-  viewSubmission(submission){
-    this.set('selectedSubmission', submission)
-    this.set('showModal', true)
+  viewSubmission(submission) {
+    this.set("selectedSubmission", submission);
+    this.set("showModal", true);
   }
 }
