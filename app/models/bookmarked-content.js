e2ea7375f7e4f137caa8e3db9a40e760d69a5f0a
Aditya <aditya2041999@gmail.com>
2021-05-29 22:26:40 +0530

    fixed route of problemType

--- app/models/bookmarked-content.js
+++ app/models/bookmarked-content.js
@@ -10,6 +10,10 @@ export default Model.extend({
   route: Ember.computed('contest', 'problem', 'contestTypeId', function() {
     const contest_type = this.contest.get('contest_type')
     switch (contest_type) {
+      case 'practice': return {
+        name: 'practice.contest.content',
+        model: [this.contestTypeId, this.content.get('id')]
+      }
       case 'dcb': return {
         name: 'dcb.problem',
         model: [this.content.get('id')]
