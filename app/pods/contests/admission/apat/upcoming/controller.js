0d8c61e8e5ec661d64b68ee168ce80e6e1d43446
Aditya <aditya2041999@gmail.com>
2021-06-07 16:56:10 +0530

    added pagination and search feature in apat admission contest

---
+++ app/pods/contests/admission/apat/upcoming/controller.js
@@ -0,0 +1,55 @@
+import Controller from '@ember/controller';
+import { action, computed } from '@ember/object'
+import { inject as service } from '@ember/service';
+import { restartableTask } from 'ember-concurrency-decorators';
+
+export default class UpcomingController extends Controller {
+  @service store
+
+  queryParams = ['offset', 'limit', 'q']
+  offset = 0
+  limit = 6
+  q = ''
+
+  @computed('offset', 'limit')
+  get page() {
+    return {
+      offset: this.offset,
+      limit: this.limit
+    }
+  }
+
+  @computed('q')
+  get filter() {
+    this.set('offset', 0)
+    if (this.q !== '')
+      return {
+        name: {
+          $iLike: `%${this.q}%`
+        }
+      }
+  }
+
+  @restartableTask fetchContestsTask = function* () {
+    try {
+      return yield this.store.query('admission-contest', {
+        page: this.page,
+        custom: {
+          ext: 'url',
+          url: 'upcoming',
+        },
+        filter:{
+            ...this.filter,
+            type:'apat'
+        }
+      })
+    } catch (err) {
+      this.set('showError', true)
+    }
+  }
+
+  @action
+  setOffset(offset) {
+    this.set('offset', offset)
+  }
+}
---
+++ tests/unit/pods/contests/admission/apat/upcoming/controller-test.js
@@ -0,0 +1,12 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Controller | contests/admission/apat/upcoming', function(hooks) {
+  setupTest(hooks);
+
+  // Replace this with your real tests.
+  test('it exists', function(assert) {
+    let controller = this.owner.lookup('controller:contests/admission/apat/upcoming');
+    assert.ok(controller);
+  });
+});
