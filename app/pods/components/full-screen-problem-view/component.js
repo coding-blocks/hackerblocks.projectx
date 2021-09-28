da73e71c2bab98cf012a041d3605fb2ac2d0b20e
Zeeshan Adeen <zeeshan99adeen@gmail.com>
2021-09-28 11:32:05 +0530

    add: copy code to editor on timed contest

--- app/pods/components/full-screen-problem-view/component.js
+++ app/pods/components/full-screen-problem-view/component.js
@@ -1,5 +1,19 @@
 import Component from '@ember/component';
+import { action } from '@ember/object';
 
 export default class FullScreenProblemView extends Component {
   currentTab = 'problem'
+  copiedSubmission = ''
+
+  @action
+  copySubmission(submission) {
+
+    /*
+      copying class instance
+      to make child component re-render when user copy the same code.
+    */
+    var sub = Object.assign(Object.create(Object.getPrototypeOf(submission)), submission);
+
+    this.set('copiedSubmission', sub);
+  }
 }
