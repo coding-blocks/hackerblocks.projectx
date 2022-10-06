9f447c3e5aba96edb8c36dbaf5a424cc2037f798
Vibhu Dujari <vdvibhu20@gmail.com>
2022-10-06 12:29:44 +0530

    add startPolling to serverTime

--- app/services/server-time.js
+++ app/services/server-time.js
@@ -1,31 +1,29 @@
 import Service from '@ember/service';
-import Moment from 'moment';
 import ENV from 'hackerblocks/config/environment';
 import { inject as service } from '@ember/service';
 import { restartableTask } from 'ember-concurrency-decorators';
 import moment from 'moment';
+import { action } from '@ember/object';
 
 export default class ServerTime extends Service {
   @service poll
   @service ajax
   
   offset = 0
+  
+  @restartableTask refreshCurrentTime = function * () {
+    const data = yield this.ajax.request(ENV.apiHost + '/time')
+    const diff = Date.now() - data.now
+    moment.now = () => {
+      return Date.now() - diff;      
+    } 
+  }
 
-  constructor() {
-    super(...arguments)
-    this.refreshCurrentTime.perform()
-    this.set('now', Moment())
+  @action
+  startPolling() {
     this.poll.addPoll({
       interval: 60000,
       callback: () => this.refreshCurrentTime.perform()
     })
-    moment.now = () => {
-      return Date.now() + this.offset;      
-    } 
-  }
-
-  @restartableTask refreshCurrentTime = function * () {
-    const data = yield this.ajax.request(ENV.apiHost + '/time')
-    this.set ('offset', Moment.unix(data.now/1000) - Date.now())
   }
 }
