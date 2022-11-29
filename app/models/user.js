3374ced4460da4932cc76ab767b2de942535f4eb
Vibhu Dujari <vdvibhu20@gmail.com>
2022-11-30 01:15:13 +0530

    redirect to oneauth if no verifiedmobile

--- app/models/user.js
+++ app/models/user.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
   name: DS.attr(),
   email: DS.attr(),
   verifiedemail: DS.attr(),
+  verifiedmobile: DS.attr(),
   course: DS.attr(),
   oauth_id: DS.attr(),
   photo: DS.attr(),
--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -2,7 +2,8 @@ import Route from '@ember/routing/route';
 import UTMCookieRouteMixin from 'hackerblocks/mixins/utm-cookie-route-mixin';
 import { inject as service } from '@ember/service';
 import { isNone } from '@ember/utils';
-import config from 'hackerblocks/config/environment'
+import config from 'hackerblocks/config/environment';
+import moment from 'moment';
 
 export default Route.extend(UTMCookieRouteMixin, {
     session: service(),
@@ -44,6 +45,12 @@ export default Route.extend(UTMCookieRouteMixin, {
     async model () {
       if (this.get('session.isAuthenticated')) {
         await this.currentUser.load()
+        if(!this.currentUser.user.verifiedmobile) {
+          document.cookie = `cb_redirect=${config.publicUrl}; expires=${moment().add(1, 'hours').toDate()}; path=/; Secure`
+          window.location = config.oneauthURL + '/users/me/edit?flow=verify_user_details'
+        } else {
+          document.cookie = `cb_redirect=${config.publicUrl}; expires=${moment().subtract(3, 'days').toDate()}`
+        }
       }
     },
 
