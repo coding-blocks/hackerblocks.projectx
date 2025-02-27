5fdae055c888d6aa2eafdaa300f47332e3e7c144
Vibhu Dujari <vdvibhu20@gmail.com>
2025-02-27 23:39:05 +0530

    Revert "Merge pull request #158 from SPARSH1608/mysql"
    This reverts commit 9d34110bbb699548006cf065752478eccdd33af0, reversing
    changes made to bd19df51dc50fd6132429c0bcaaccccd56ddeebc.

--- app/helpers/json-to-table.js
+++
@@ -1,6 +0,0 @@
-import { helper } from '@ember/component/helper';
-import convertToTable from '../util/json-to-table'; // Ensure correct path
-
-export default helper(function jsonToTable([data]) {
-    return convertToTable(data);
-});
