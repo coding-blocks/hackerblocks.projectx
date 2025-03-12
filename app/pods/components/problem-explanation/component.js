569b40c98c75232ba054fdd1482377294c4560fb
SPARSH1608 <sparshgoelk@gmail.com>
2025-03-12 17:05:23 +0530

    disabling copy actions and code inspection shortcuts

--- app/pods/components/problem-explanation/component.js
+++ app/pods/components/problem-explanation/component.js
@@ -1,6 +1,6 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-import {jsonToTable} from '../../../util/json-to-table'
+import { jsonToTable } from '../../../util/json-to-table';
 
 export default Component.extend({
   languageSelection: service('language-selection'),
@@ -8,7 +8,26 @@ export default Component.extend({
   init() {
     this._super(...arguments);
     let contest = this.get('contest');
+
+   
+    document.addEventListener('contextmenu', function (event) {
+      event.preventDefault();
+    });
+
+   
+    document.addEventListener('keydown', function (event) {
+      if (
+        event.key === 'F12' || 
+        (event.ctrlKey && event.shiftKey && event.key === 'I') || 
+        (event.ctrlKey && event.shiftKey && event.key === 'C') || 
+        (event.ctrlKey && event.shiftKey && event.key === 'J') || 
+        (event.ctrlKey && event.key === 'U') 
+      ) {
+        event.preventDefault();
+      }
+    });
   },
+
   jsonToTable(data) {
     const table = jsonToTable(data);
     return table;
